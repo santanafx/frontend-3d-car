902b499df8cfcfcd39b5d8c1e8e9ff3dbb2d5c0e
Lucas Santana <santanafx@hotmail.com>
2023-09-20 16:44:08 -0300

    feat: adicionado 5 botões e simulação das rotas mostrando o melhor caminho segundo o mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,19 @@ function App() {
   const [coords, setCoords] = useState([])
   const [routes1, setRoutes1] = useState([])
   const [routes1Start, setRoutes1Start] = useState(false)
-  const [currentLocationIndex, setCurrentLocationIndex] = useState(0)
+  const [routes2, setRoutes2] = useState([])
+  const [routes2Start, setRoutes2Start] = useState(false)
+  const [routes3, setRoutes3] = useState([])
+  const [routes3Start, setRoutes3Start] = useState(false)
+  const [routes4, setRoutes4] = useState([])
+  const [routes4Start, setRoutes4Start] = useState(false)
+  const [routes5, setRoutes5] = useState([])
+  const [routes5Start, setRoutes5Start] = useState(false)
+  const [currentLocationIndex1, setCurrentLocationIndex1] = useState(0)
+  const [currentLocationIndex2, setCurrentLocationIndex2] = useState(0)
+  const [currentLocationIndex3, setCurrentLocationIndex3] = useState(0)
+  const [currentLocationIndex4, setCurrentLocationIndex4] = useState(0)
+  const [currentLocationIndex5, setCurrentLocationIndex5] = useState(0)
 
   useEffect(() => {
     getRoute()
@@ -46,18 +58,78 @@ function App() {
     console.log(data.courses)
 
     setRoutes1(data.courses[0].gps)
+    setRoutes2(data.courses[1].gps)
+    setRoutes3(data.courses[2].gps)
+    setRoutes4(data.courses[3].gps)
+    setRoutes5(data.courses[4].gps)
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (currentLocationIndex < routes1.length - 1) {
-        setCurrentLocationIndex(currentLocationIndex + 1)
-        console.log(currentLocationIndex)
+      if (currentLocationIndex1 < routes1.length - 1) {
+        setCurrentLocationIndex1(currentLocationIndex1 + 1)
+        console.log(currentLocationIndex1)
+      } else if (currentLocationIndex1 === routes1.length) {
+        setRoutes2Start(!routes1Start)
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [currentLocationIndex1, routes1Start])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentLocationIndex2 < routes2.length - 1) {
+        setCurrentLocationIndex2(currentLocationIndex2 + 1)
+        console.log(currentLocationIndex2)
+      } else if (currentLocationIndex2 === routes2.length) {
+        setRoutes2Start(!routes2Start)
       }
     }, 1000)
 
     return () => clearInterval(interval)
-  }, [currentLocationIndex, routes1Start])
+  }, [currentLocationIndex2, routes2Start])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentLocationIndex3 < routes3.length - 1) {
+        setCurrentLocationIndex3(currentLocationIndex3 + 1)
+        console.log(currentLocationIndex3)
+      } else if (currentLocationIndex3 === routes3.length) {
+        setRoutes2Start(!routes3Start)
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [currentLocationIndex3, routes3Start])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentLocationIndex4 < routes4.length - 1) {
+        setCurrentLocationIndex4(currentLocationIndex4 + 1)
+        console.log(currentLocationIndex4)
+      } else if (currentLocationIndex4 === routes4.length) {
+        setRoutes2Start(!routes4Start)
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [currentLocationIndex4, routes4Start])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentLocationIndex5 < routes5.length - 1) {
+        setCurrentLocationIndex5(currentLocationIndex5 + 1)
+        console.log(currentLocationIndex5)
+      } else if (currentLocationIndex5 === routes5.length) {
+        setRoutes2Start(!routes5Start)
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [currentLocationIndex5, routes5Start])
 
   const geojson = {
     type: 'FeatureCollection',
@@ -107,15 +179,40 @@ function App() {
       data: end
     },
     paint: {
-      'circle-radius': 30,
+      'circle-radius': 10,
       'circle-color': '#f30'
     }
   }
 
   const startRoute1 = () => {
+    setCurrentLocationIndex1(0)
     setStart([-46.28054, -23.963214])
     setEnd([-46.278736, -23.913536])
     setRoutes1Start(!routes1Start)
+  }
+  const startRoute2 = () => {
+    setCurrentLocationIndex2(0)
+    setStart([-46.278702, -23.913509])
+    setEnd([-46.280632, -23.963248])
+    setRoutes2Start(!routes2Start)
+  }
+  const startRoute3 = () => {
+    setCurrentLocationIndex3(0)
+    setStart([-46.280566, -23.963217])
+    setEnd([-46.278736, -23.913536])
+    setRoutes3Start(!routes3Start)
+  }
+  const startRoute4 = () => {
+    setCurrentLocationIndex4(0)
+    setStart([-46.282916, -23.964503])
+    setEnd([-46.265922, -23.973034])
+    setRoutes4Start(!routes4Start)
+  }
+  const startRoute5 = () => {
+    setCurrentLocationIndex5(0)
+    setStart([-46.265751, -23.973013])
+    setEnd([-46.27787, -23.963164])
+    setRoutes5Start(!routes5Start)
   }
 
   return (
@@ -130,15 +227,95 @@ function App() {
         <GeolocateControl />
         <FullscreenControl />
         <NavigationControl />
-        {currentLocationIndex < routes1.length && (
+
+        {currentLocationIndex1 < routes1.length && routes1Start === true && (
+          <>
+            <Marker
+              longitude={routes1[currentLocationIndex1].longitude}
+              latitude={routes1[currentLocationIndex1].latitude}
+            >
+              <div className="marker">📍</div>
+            </Marker>
+
+            <Source id="routeSource" type="geojson" data={geojson}>
+              <Layer {...lineStyle} />
+            </Source>
+
+            <Source id="endSource" type="geojson" data={endPoint}>
+              <Layer {...layerEndpoint} />
+            </Source>
+          </>
+        )}
+
+        {currentLocationIndex2 < routes2.length && (
           <Marker
-            longitude={routes1[currentLocationIndex].longitude}
-            latitude={routes1[currentLocationIndex].latitude}
+            longitude={routes2[currentLocationIndex2].longitude}
+            latitude={routes2[currentLocationIndex2].latitude}
           >
             <div className="marker">📍</div>
           </Marker>
         )}
-        {routes1Start === true && (
+        {routes2Start === true && (
+          <>
+            <Source id="routeSource" type="geojson" data={geojson}>
+              <Layer {...lineStyle} />
+            </Source>
+
+            <Source id="endSource" type="geojson" data={endPoint}>
+              <Layer {...layerEndpoint} />
+            </Source>
+          </>
+        )}
+
+        {currentLocationIndex3 < routes3.length && (
+          <Marker
+            longitude={routes3[currentLocationIndex3].longitude}
+            latitude={routes3[currentLocationIndex3].latitude}
+          >
+            <div className="marker">📍</div>
+          </Marker>
+        )}
+        {routes3Start === true && (
+          <>
+            <Source id="routeSource" type="geojson" data={geojson}>
+              <Layer {...lineStyle} />
+            </Source>
+
+            <Source id="endSource" type="geojson" data={endPoint}>
+              <Layer {...layerEndpoint} />
+            </Source>
+          </>
+        )}
+
+        {currentLocationIndex4 < routes4.length && (
+          <Marker
+            longitude={routes4[currentLocationIndex4].longitude}
+            latitude={routes4[currentLocationIndex4].latitude}
+          >
+            <div className="marker">📍</div>
+          </Marker>
+        )}
+        {routes4Start === true && (
+          <>
+            <Source id="routeSource" type="geojson" data={geojson}>
+              <Layer {...lineStyle} />
+            </Source>
+
+            <Source id="endSource" type="geojson" data={endPoint}>
+              <Layer {...layerEndpoint} />
+            </Source>
+          </>
+        )}
+
+        {currentLocationIndex5 < routes5.length && (
+          <Marker
+            longitude={routes5[currentLocationIndex5].longitude}
+            latitude={routes5[currentLocationIndex5].latitude}
+          >
+            <div className="marker">📍</div>
+          </Marker>
+        )}
+        {routes5Start === true && (
           <>
             <Source id="routeSource" type="geojson" data={geojson}>
               <Layer {...lineStyle} />
@@ -151,6 +328,10 @@ function App() {
         )}
       </Map>
       <button onClick={startRoute1}>Iniciar rota 1</button>
+      <button onClick={startRoute2}>Iniciar rota 2</button>
+      <button onClick={startRoute3}>Iniciar rota 3</button>
+      <button onClick={startRoute4}>Iniciar rota 4</button>
+      <button onClick={startRoute5}>Iniciar rota 5</button>
     </>
   )
 }
