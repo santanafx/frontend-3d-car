3c6efe210681e6a39e36fa0dbe5c2d2045777ab7
lucas <santanafx@hotmail.com>
2025-04-26 07:28:33 -0300

    feat: add .env file for Mapbox access token and refactor BestRoute component

--- src/components/BestRoute/index.tsx
+++ src/components/BestRoute/index.tsx
@@ -1,40 +1,42 @@
-import { Source, Layer } from 'react-map-gl'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from "react";
+import { Layer, Source } from "react-map-gl";
+
+const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN as string;
 
 type Props = {
-  start: number[]
-  end: number[]
-}
+  start: number[];
+  end: number[];
+};
 
 export default function BestRoute({ start, end }: Props) {
-  const [coords, setCoords] = useState<number[][]>([])
+  const [coords, setCoords] = useState<number[][]>([]);
 
   useEffect(() => {
-    getRoute()
-  }, [])
+    getRoute();
+  }, []);
 
   const getRoute = async () => {
     const response = await fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=pk.eyJ1Ijoic2FudGFuYWZ4IiwiYSI6ImNsbXF2aXFhdDAxaDAyaXBqMXRvb2IxNnIifQ.tqESIHRN6mDXgVvKEVeQRQ`
-    )
-    const data = await response.json()
-    const coords = data.routes[0].geometry.coordinates
-    setCoords(coords)
-  }
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${MAPBOX_ACCESS_TOKEN}`
+    );
+    const data = await response.json();
+    const coords = data.routes[0].geometry.coordinates;
+    setCoords(coords);
+  };
 
   const geojson: GeoJSON.FeatureCollection<GeoJSON.LineString> = {
-    type: 'FeatureCollection',
+    type: "FeatureCollection",
     features: [
       {
-        type: 'Feature',
+        type: "Feature",
         geometry: {
-          type: 'LineString',
-          coordinates: coords
+          type: "LineString",
+          coordinates: coords,
         },
-        properties: {}
-      }
-    ]
-  }
+        properties: {},
+      },
+    ],
+  };
 
   return (
     <>
@@ -42,14 +44,14 @@ export default function BestRoute({ start, end }: Props) {
         <Layer
           id="roadLayer"
           type="line"
-          layout={{ 'line-join': 'round', 'line-cap': 'round' }}
+          layout={{ "line-join": "round", "line-cap": "round" }}
           paint={{
-            'line-color': 'black',
-            'line-width': 7,
-            'line-opacity': 0.8
+            "line-color": "black",
+            "line-width": 7,
+            "line-opacity": 0.8,
           }}
         />
       </Source>
     </>
-  )
+  );
 }
